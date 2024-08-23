89cd1e5c93e06f42f2d6cab78a5a7a8784395ab4
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2024-08-23 15:06:57 +0700

    first commitcommit

---
+++ src/BaseLineIEEE/MainBaseLineIEEE.java
@@ -0,0 +1,60 @@
+package BaseLineIEEE;
+
+import BaseLine.AlgoTopKINC;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+
+public class MainBaseLineIEEE {
+
+	public static void main(String[] arg) throws IOException {
+
+		String input = "DB.txt";
+		String output = ".//output.txt";
+		int k=15;
+
+		// the number of updates to be performed
+		int numberOfUpdates = 1;
+
+		// scan the database to count the number of lines
+		// for our test purpose
+		int linecount = countLines(input);
+
+		double addedratio = 1d / ((double) numberOfUpdates);
+		int linesForeEachUpdate = (int) (addedratio * linecount);
+
+		// Apply the algorithm several times
+		AlgoTHUI algo = new AlgoTHUI();
+		int firstLine = 0;
+		for (int i = 0; i < numberOfUpdates; i++) {
+			int lastLine = firstLine + linesForeEachUpdate;
+			if (i == numberOfUpdates - 1) {
+				System.out.println("" + i + ") Run the algorithm using line " + firstLine + " to before line "
+						+ linecount + " of the input database.");
+				algo.runAlgorithm(input,output,false, k, firstLine, linecount);
+			}
+			algo.printStats();
+
+			firstLine = lastLine;
+		}
+
+	}
+
+	/**
+	 * This methods counts the number of lines in a text file.
+	 * 
+	 * @param filepath the path to the file
+	 * @return the number of lines as an int
+	 * @throws IOException Exception if error reading/writting file
+	 */
+	public static int countLines(String filepath) throws IOException {
+		LineNumberReader reader = new LineNumberReader(new FileReader(filepath));
+		while (reader.readLine() != null) {
+		}
+		int count = reader.getLineNumber();
+		reader.close();
+		return count;
+	}
+
+}
